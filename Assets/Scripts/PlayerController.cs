9b492b52c29a567848afc9059d2f726a8ad4d357
VIKTOR <vikt.gustafsson@gmail.com>
2017-11-05 20:10:02 +0100

    Maze-Building

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private Rigidbody rb;
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private int Count;
+	private Rigidbody rb;
 
 	void Start ()
 	{
+		Count = 0;
+		SetCountText ();
+		winText.text = "";
+
 		rb = GetComponent<Rigidbody> ();	
 	}
 
@@ -21,5 +29,23 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce(movement*speed);
 	}
+		
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			Count++;
+			SetCountText ();
+		}
+	}
 
+	void SetCountText () {
+		countText.text = "Count: " + Count.ToString();
+
+		if (Count >= 11)
+		{
+			winText.text = "You Win!";
+		}
+	}
 }
