324f25a5159a028c3d31638c2f6bd9546fa63108
VIKTOR <vikt.gustafsson@gmail.com>
2017-11-08 17:39:28 +0100

    Commit again?

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Time.deltaTime;
+		transform.Rotate (new Vector3 (90, 180, 30)* translation);
+	}
+}
