4f4e7a56bdd1e96c37492813dd635a01e10e65e8
Lucas Rodrigues <rod.luk@gmail.com>
2017-11-08 16:59:41 +0100

    Added home screen interaction

---
+++ Assets/CanvasData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasData : MonoBehaviour {
+
+	private int chosenPlayerInt;
+	public Text playerMenuItemText;
+	static string[] playerNames = new string[] {
+		"Speedy Ballzales",
+		"Ballzy Jumper"
+	};
+
+	public void loadPlayerPrefs(){
+		/* 
+		 * Loading settings from PlayerPrefs
+		 * If player is not set, it gets defined as 1 / Speedy Ballzales
+		 */
+		if (PlayerPrefs.GetInt ("player") != null) {
+			chosenPlayerInt = PlayerPrefs.GetInt ("player");
+		} else {
+			chosenPlayerInt = 0;
+		}
+	}
+
+	void Update(){
+		loadPlayerPrefs ();
+		playerMenuItemText.text = "Player: " + playerNames [chosenPlayerInt];
+	}
+
+}
