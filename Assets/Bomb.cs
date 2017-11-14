5be75eb0628347f48d4b91e0d6246154db306d16
Lucas Rodrigues <rod.luk@gmail.com>
2017-11-15 00:20:20 +0100

    Added difficulty

---
+++ Assets/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	public AudioClip bombClip;
+	private AudioSource source;
+
+	void Awake () {
+
+		source = GetComponent<AudioSource>();
+
+	}
+
+	void OnTriggerExit(Collider other){
+		source.PlayOneShot(bombClip, 1F);
+	}
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,70 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
-	public bool speedBoost;
-	public bool jumpBoost;
-	public Text countText;
-	public Text winText;
-	public float timer;
-	public float jumpValue;
+	// Player characteristic default values
+	private int speed;
+	private int jump;
+
+	static int number_of_coins = 20;
+
+	// Variables related to player preferences
+	private int player;
+	private int difficulty;
+
+	// Set-up of player 1 (Speedy Ballzales) characteristics
+	static Color colorPlayerOne = Color.blue;
+	static int speedPlayerOne = 20;
+	static int jumpPlayerOne = 200;
+
+	// Set-up of player 2 (Ballsy Jumper) characteristics
+	static Color colorPlayerTwo = Color.red;
+	static int speedPlayerTwo = 10;
+	static int jumpPlayerTwo = 400;
 
 	private int Count;
-	private Rigidbody rb;
+	public Rigidbody rb;
 	private Color playerColor;
+
+	public bool speedBoost;
+	public bool jumpBoost;
+	public float gameTimer;
+	public float timer;
+
+	public Text displayTime;
+	public Text youHaveSpeed;
+	public Text youHaveJump;
+	public Text countText;
+	public Text winText;
+
+	public AudioClip bombClip;
+	private AudioSource source;
 
 	void Start ()
 	{
+		// Getting the player preferences
+		difficulty = PlayerPrefs.GetInt ("difficulty");
+		player = PlayerPrefs.GetInt ("player");
+
+		// Setting game according to Player preferences
+		setDifficulty(difficulty);
+		setPlayer(player);
+
+		displayTime.text = "";
 		Count = 0;
-		jumpValue = 200.0f;
-		SetCountText ();
+		SetCountText();
 		winText.text = "";
 
+		speedBoost = false;
+
+		youHaveJump.text = "";
+		youHaveSpeed.text = "";
+		speed = speedPlayerOne;
+
 		rb = GetComponent<Rigidbody> ();	
-		playerColor = GetComponent<Renderer> ().material.color;
+		playerColor = GetComponent<Renderer>().material.color;
+
+		//source = GetComponent<AudioSource>();
+
 	}
 
 	void FixedUpdate () 
@@ -32,22 +75,30 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 jump = new Vector3 (0.0f, jumpValue, 0.0f);
+		SetGameTime ();
+
+		Vector3 jumpAction = new Vector3 (0.0f, jump, 0.0f);
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 	
-		rb.AddForce(movement*speed);
+		rb.AddForce (movement * speed);
 
 		// Jumper
 		if (Input.GetKeyDown("space") && rb.transform.position.y <= 0.5f) {
-			rb.AddForce (jump);
-		} 
+			rb.AddForce (jumpAction);
+		}
+
 		// Speed booster
 		else if (Input.GetKeyDown("s")) {
-			if (speed == 50.0) {
-				speed = 10;
-			} else {
+			if (speedBoost) {
 				speed = 50;
+			} else {
+				speed = 10;
 			}
+		}
+
+		// STOP function
+		else if (Input.GetKeyDown ("b")) {
+			rb.velocity = Vector3.zero;
 		}
 			
 	}
@@ -58,31 +109,44 @@ public class PlayerController : MonoBehaviour {
 		{
 			Count++;
 			SetCountText ();
-			//other.gameObject.SetActive(false);
+			other.gameObject.SetActive(false);
 
 		} 
 		else if (other.gameObject.CompareTag("jumpPickUp"))
 		{
-			jumpValue = 400.0f;
+			jump = 400;
 			GetComponent<Renderer> ().material.color = Color.yellow;
+			youHaveJump.text = "Press SPACE for temporary Super Jump!";
+			Invoke("youHaveJumpText", 3);
 			Invoke("endJumpBooster", 10f);
 			other.gameObject.SetActive(false);
 
 		} 
 		else if (other.gameObject.CompareTag("speedPickUp")) 
 		{
-			speed = 50;
+			speedBoost = true;
 			GetComponent<Renderer> ().material.color = Color.red;
+			youHaveSpeed.text = "Press S for temporary Super Speed!";
+			Invoke ("youHaveSpeedText", 3);
 			Invoke ("endSpeedBooster", 5f);
 			other.gameObject.SetActive (false);
+		}
+
+		else if (other.gameObject.CompareTag("bombTrap")) 
+		{
+			source = other.gameObject.GetComponent<AudioSource>();
+			source.PlayOneShot(bombClip, 1F);
+			rb.AddForce (new Vector3 (0.0f, 500, 0.0f));
 		}
 
 	}
 
 	void SetCountText () {
-		countText.text = "Count: " + Count.ToString();
+		//Shows the number of coins that are left to be picked-up
+		countText.text = "Remaining \nCoins: " + (number_of_coins - Count).ToString();
 
-		if (Count >= 11)
+		//Checking if all the coins have been picked up
+		if (Count >= number_of_coins)
 		{
 			winText.text = "You Win!";
 		}
@@ -94,7 +158,47 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void endJumpBooster () {
+		speedBoost = false;
 		rb.GetComponent<Renderer> ().material.color = playerColor;
-		jumpValue = 200.0f;
+		jump = 200;
+	}
+
+	void SetGameTime () {
+		gameTimer = Time.time;	
+		displayTime.text = "Time: " + gameTimer.ToString();
+	}
+
+	void setDifficulty(int difficulty) {
+		if (difficulty == 1) {
+			GameObject[] goArray = GameObject.FindGameObjectsWithTag("bombTrap");
+			Debug.Log(goArray.Length);
+
+			if (goArray.Length > 0) {
+				for (int i = 0; i < goArray.Length; i++) {
+					goArray[i].GetComponent<MeshRenderer>().enabled = true;
+					goArray[i].GetComponent<BoxCollider> ().enabled = true;
+				}
+			}
+		}
+	}
+
+	void setPlayer(int player) {
+		if (player == 0) {
+			rb.GetComponent<Renderer>().material.color = colorPlayerOne;
+			speed = speedPlayerOne;
+			jump = jumpPlayerOne;
+		} else if (player == 1) {
+			rb.GetComponent<Renderer>().material.color = colorPlayerTwo;
+			speed = speedPlayerTwo;
+			jump = jumpPlayerTwo;
+		}
+	}
+
+	void youHaveJumpText() {
+		youHaveJump.text = "";
+	}
+
+	void youHaveSpeedText() {
+		youHaveSpeed.text = "";
 	}
 }
